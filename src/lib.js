a6a26c01466acda106620974b475a9d24c28f2d5
markmendozadev <markmendozadev@gmail.com>
2022-04-06 11:25:51 +0800

    updates

--- src/lib.js
+++ src/lib.js
@@ -3,7 +3,7 @@ export const getIpAddress = async (ipAddress) => {
   const parameters = ipAddress ? { ipAddress: ipAddress } : {};
   const response = await axios("https://geo.ipify.org/api/v2/country,city", {
     params: {
-      apiKey: "at_coQ3FTupGaKnWVtDlc9bxWorjHw0I",
+      apiKey: process.env.REACT_APP_API_KEY,
       ...parameters,
     },
   });
